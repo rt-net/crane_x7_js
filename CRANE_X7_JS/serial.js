defd5f2fa35666f24f2f4aa2e58368ef55806812
RikiHayashi <s1526091XB@s.chibakoudai.jp>
2019-07-31 16:48:45 +0900

    add reference

--- CRANE_X7_JS/serial.js
+++ CRANE_X7_JS/serial.js
@@ -13,6 +13,11 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
+   @REFERENCE
+       https://github.com/ROBOTIS-GIT/DynamixelSDK
+   
+   This code is based on the Dynamixel SDK source code
  */
 var SerialPort = require('serialport');
 
